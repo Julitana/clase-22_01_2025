9bd4a035670cd8bf6d3ec1648d7fcbdc29efeb69
Julitana <aangulo@vlacademy.com>
2025-01-22 21:01:07 -0600

    Funcion con DOM

---
+++ funciones.js
@@ -0,0 +1,24 @@
+console.log("Funcionando")
+// Zona de declaracion de Variables Globales
+
+
+
+
+
+
+
+//Zona para llamar funciones 
+
+
+
+
+
+
+
+
+//Zona de funciones 
+function SaludarconNombre() {
+    console.log("Funcionando SaludarconNombre")
+    let IDNombre = document.getElementById("IDNombre").value;
+    console.log("IDNombre")
+}
